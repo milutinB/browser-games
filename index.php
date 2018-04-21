79afc23273e968982fd21be1ce701eccf25f1473
Milutin Brankovic <milutin.brankovic3@gmail.com>
2018-04-21 20:18:13 +1000

    test

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 include 'HelloController.php';
 
+
 /*if($_SERVER["HTTPS"] != "on")
 {
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     exit();
 }*/
 
-
+header("Location: databaseTest.php");
 
 $r = $_SERVER["REQUEST_URI"];
 
-echo $r;
+/*echo $r;/
 
 //$controller;
 /*
