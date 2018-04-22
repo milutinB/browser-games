92b792aa715161dfb99d0d7a892d3cb2f28c213c
Milutin Brankovic <milutin.brankovic3@gmail.com>
2018-04-22 16:18:26 +1000

    test

--- index.php
+++ index.php
@@ -4,12 +4,6 @@ include 'src/hello.html;';
 
 //echo 'Hi';
 
-if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) { 
-    $url = 'https://' . $_SERVER['HTTP_HOST']
-                      . $_SERVER['REQUEST_URI'];
-    header('Location: ' . $url);
-    die();
-}
 
 $request = new Request($_GET, $_POST, $_SERVER["REQUEST_URI"]);
 
