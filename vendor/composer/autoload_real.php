e63c14073eebbbb5f0e1bc3901d8d638e1cf12f5
Milutin Brankovic <milutin.brankovic3@gmail.com>
2018-05-07 19:26:19 +1000

    game running

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInit9a4bec0382fd5e7056497405ebc04eb0
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit9a4bec0382fd5e7056497405ebc04eb0::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire9a4bec0382fd5e7056497405ebc04eb0($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire9a4bec0382fd5e7056497405ebc04eb0($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
